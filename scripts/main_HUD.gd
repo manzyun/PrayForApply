c9aad0c232c7febc192b785935b81d4874465473
Ittetsu Yoshino <yoshino.ittetsu.0427@gmail.com>
2022-04-30 20:29:32 +0900

    add FlameGauge

--- scripts/main_HUD.gd
+++ scripts/main_HUD.gd
@@ -3,8 +3,14 @@ extends CanvasLayer
 signal generate_job_seeker
 
 func _ready():
-    pass
+	pass
 
 func _on_GenerateButton_pressed():
-    emit_signal("generate_job_seeker")
-    pass
+	emit_signal("generate_job_seeker")
+	pass
+
+func _process(delta):
+	update_flame_progress(50)
+	
+func update_flame_progress(flame_point):
+	$FlameProgress.value = flame_point
