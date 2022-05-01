b20da8dc53193447baeef358dfdb0ed91999ebfd
AIPON4 <72207793+AIPON4@users.noreply.github.com>
2022-05-01 16:37:25 +0900

    Add scripts and scenes.
    うごくようになりました．

--- scripts/ListedProgress.gd
+++ scripts/ListedProgress.gd
@@ -1,23 +1,12 @@
 extends TextureProgress
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 const listed_icon_size_x = 66
 const listed_icon_left_offset = 20
 const listed_icon_right_offset = 110
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+    pass
 
 func _process(delta):
-	$ListedIcon.position.x = listed_icon_size_x + listed_icon_left_offset \
-						   + value * ((rect_size.x - listed_icon_size_x - listed_icon_right_offset) / 100) 
+    $ListedIcon.position.x = listed_icon_size_x + listed_icon_left_offset \
+                           + value * ((rect_size.x - listed_icon_size_x - listed_icon_right_offset) / 100) 
