a6acc0ea28087300733439c2bbd21dc1333a35d0
AIPON4 <72207793+AIPON4@users.noreply.github.com>
2022-05-01 18:14:35 +0900

    Add blacklist?
    ブラックリストの内部的な処理だけできた気がします．
    値が完全に決め打ちなので拡張性が...

--- scripts/Applyer.gd
+++ scripts/Applyer.gd
@@ -26,5 +26,70 @@ func _generate_applyer_graphics(var dict):
         animated_sprite_body.frame = body
         head = rng.randi_range(8, 15)
         animated_sprite_head.frame = head
+        
+    if applyer_status["edu"] == "アート専門学校 3Dアーティストコース 卒業":
+        head = -1
+        body = -1
+        pass
+        
+    if applyer_status["edu"] == "アート専門学校 イラストレーターコース 卒業":
+        head = -1
+        body = -1
+        pass
+        
+    if applyer_status["edu"] == "アート専門学校 デジタルサウンドコース 卒業":
+        head = -1
+        body = -1
+        pass
+        
+    if applyer_status["edu"] == "デジタル専門学校 卒業":
+        head = -1
+        body = -1
+        pass
+        
+    if applyer_status["edu"] == "芸術大学 芸術学部 卒業":
+        head = -1
+        body = -1
+        pass
+        
+    if applyer_status["workhis"] == "未経験":
+        head = -1
+        body = -1
+        pass
+        
+    if applyer_status["workhis"] == "プログラマー 経験年数3年以上":
+        head = -1
+        body = -1
+        pass
     
+    if applyer_status["workhis"] == "プログラマー 経験年数10年以上":
+        head = -1
+        body = -1
+        pass
+    
+    if applyer_status["workhis"] == "ゲーム会社勤務(PG)":
+        head = -1
+        body = -1
+        pass
+    
+    if applyer_status["workhis"] == "2Ｄデザイン 経験年数10年以上":
+        head = -1
+        body = -1
+        pass
+        
+    if applyer_status["license"] == "色彩検定2級":
+        head = -1
+        body = -1
+        pass
+        
+    if applyer_status["license"] == "CAD技能検定一級":
+        head = -1
+        body = -1
+        pass
+    
+    if applyer_status["license"] == "blender検定一級":
+        head = -1
+        body = -1
+        pass
+           
     emit_signal("graphics_pare", body, head)
